1e1829e1547f8fd11076aaf0178e6bef775426b3
Breanna Burris <breanna1burris@gmail.com>
2024-09-09 16:20:42 -0400

    Initializing inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,12 @@
+// Tasks 1: Create an inventory array of Product Objects
+
+
+// Initializing inventory with product objects
+let inventory = [
+{ name:"Laptop", price: 2000, quantity: 500, lowStockLevel: 200 }, // Laptop for $2000, 500 in stock and minimum stock level of 200
+{ name: "Phone", price: 1000, quantity: 100, lowStockLevel: 50 }, //Phone for $1000, 100 in stock and minimum stock level of 50
+{ name: "Tablet", price: 800, quantity: 70, lowStockLevel: 30 }, //Tablet for $800, 70 in stock and minimum stock level of 30
+{ name: "TV", price: 1500, quantity: 80, lowStockLevel: 8 }, //TV for $1500, 80 in stock and minimum stock level of 8
+{ name: "Speaker", price: 300, quantity: 20, lowStockLevel: 10 } // Speaker for $300, 20 in stock and minimum stock level of 10
+];
+     
