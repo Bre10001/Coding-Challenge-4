8797b7e284756a466cadb76bb3e77d07f7755d3f
Breanna Burris <breanna1burris@gmail.com>
2024-09-14 18:15:08 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -1,12 +1,21 @@
-// Tasks 1: Create an inventory array of Product Objects
+// Task 1: Create an inventory array of Product Objects
 
 
 // Initializing inventory with product objects
-let inventory = [
-{ name:"Laptop", price: 2000, quantity: 500, lowStockLevel: 200 }, // Laptop for $2000, 500 in stock and minimum stock level of 200
-{ name: "Phone", price: 1000, quantity: 100, lowStockLevel: 50 }, //Phone for $1000, 100 in stock and minimum stock level of 50
-{ name: "Tablet", price: 800, quantity: 70, lowStockLevel: 30 }, //Tablet for $800, 70 in stock and minimum stock level of 30
-{ name: "TV", price: 1500, quantity: 80, lowStockLevel: 8 }, //TV for $1500, 80 in stock and minimum stock level of 8
-{ name: "Speaker", price: 300, quantity: 20, lowStockLevel: 10 } // Speaker for $300, 20 in stock and minimum stock level of 10
+const inventory = [
+    { name: "Laptop", price: 2000, quantity: 500, lowStockLevel: 200 }, // Laptop for $2000, 500 in stock and minimum stock level of 200
+    { name: "Phone", price: 1000, quantity: 100, lowStockLevel: 50 }, //Phone for $1000, 100 in stock and minimum stock level of 50
+    { name: "Tablet", price: 800, quantity: 70, lowStockLevel: 30 }, //Tablet for $800, 70 in stock and minimum stock level of 30
+    { name: "TV", price: 1500, quantity: 80, lowStockLevel: 8 }, //TV for $1500, 80 in stock and minimum stock level of 8
+    { name: "Speaker", price: 300, quantity: 20, lowStockLevel: 10 } // Speaker for $300, 20 in stock and minimum stock level of 10
 ];
-     
+
+// Task 2: Function to Display Product Details
+
+function displayProductDetails(productDetails) {
+    let quantity = inventory.quantity; // declaring the quantity variable
+    const stockStatus = quantity <= productDetails.lowStockLevel ? "Low Stock" : "In Stock"; //Determining the stock status based on the quantity amount in stock
+    console. log(`Product: ${productDetails.name}\nPrice: $${productDetails.price}\nQuantity: ${productDetails.quantity}\nStock Status: ${stockStatus}`); // displays the product, price, quantity and stock status
+    
+};
+inventory.forEach(productDetails => displayProductDetails(productDetails)); // displays for each product in the inventory array
